93aa3591d35a2bcd1cf4d01f5fc049a3c6de1d79
Brian Wells <brian@bcwells.net>
2016-08-26 07:24:37 -0700

    Get working HTML and PDF links, publish.el, doc/

---
+++ publish.el
@@ -0,0 +1,32 @@
+(require 'ox-publish)
+
+(setq org-publish-project-alist
+      `(("website-html"
+	 :base-directory ,(concat default-directory "src/")
+	 :base-extension "org"
+	 :exclude "software"
+	 :publishing-directory ,(concat default-directory "doc/")
+	 :publishing-function org-html-publish-to-html
+	 :section-numbers nil
+	 :recursive t)
+	("website-pdf"
+	 :base-directory ,(concat default-directory "src/articles/")
+	 :base-extension "org"
+	 :publishing-directory ,(concat default-directory "doc/articles/")
+	 :publishing-function org-latex-publish-to-pdf
+	 :section-numbers nil)
+	("website-software"
+	 :base-directory ,(concat default-directory "src/software/")
+	 :base-extension "org\\|html\\|pdf"
+	 :publishing-directory ,(concat default-directory "doc/software/")
+	 :publishing-function org-publish-attachment
+	 :recursive t)
+	("website-root"
+	 :base-directory ,(concat default-directory "src/")
+	 :base-extension "org\\|setup"
+	 :publishing-directory ,(concat default-directory "doc/")
+	 :publishing-function org-publish-attachment)
+	("website" :components ("website-root" "website-software"
+				"website-html" "website-pdf"))))
+
+(org-publish "website")
